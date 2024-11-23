5b25b75083a01c062c88ea04ad302880122148d1
neojarvis <techadmin@iamneo.ai>
2024-11-23 14:59:55 +0000

    Auto-save-commit-23/11/2024--20:29:55

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -4,12 +4,6 @@ class Program
 {
     public static void Main()
     {
-        for(int i = 0;i<=5; i++)
-        {
-            for(int j = 1; j<=5;j++){
-                Console.Write("* ");
-            }
-            Console.WriteLine("*");
-        }
+        
     }
 }
