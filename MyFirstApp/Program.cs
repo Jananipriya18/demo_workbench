cda16fe3ae20f51275c7a1d261ff7778a1b4819d
neojarvis <techadmin@iamneo.ai>
2024-11-23 12:44:55 +0000

    Auto-save-commit-23/11/2024--18:14:55

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using System;
+class Program
+{
+    public static void Main()
+    {
+        //Arithmetic Operators
+        int a= 10, b= 10;
+        Console.WriteLine("Arithmetic Operators:");
+    }
+}
