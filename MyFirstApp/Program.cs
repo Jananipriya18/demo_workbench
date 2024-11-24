65aa391e5768f5e4bb82ac599fb5fb17a3c534d1
neojarvis <techadmin@iamneo.ai>
2024-11-24 03:45:24 +0000

    Auto-save-commit-24/11/2024--09:15:24

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Diagnostics.Metrics;
-class program{
-    static void Main()
-    {
-        Func<int,int,int> add = (a,b) => a+b;
 
-        int result = add(5,10);
-        Console.WriteLine("Sum: "+result);
+class Program{
+    public static void Main()
+    {
+        List<string> shoppingList = new List<string> {"Milk","Eggs","Bread"};
+        
     }
 }
