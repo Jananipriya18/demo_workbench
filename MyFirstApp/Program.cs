b0a2ab5f5b3cbff465db9fe3b42c6190fa66e4bb
neojarvis <techadmin@iamneo.ai>
2024-11-26 12:34:51 +0000

    Auto-save-commit-26/11/2024--18:04:50

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -8,6 +8,27 @@ class Program
     {
         List<int> numbers = new List<int>
         {10,1,7,9,2,5,3,4,8};
+
+        // Sort Ascending Order
+        var sortedNumbers = numbers.OrderBy(num=>num);
+        foreach(var num in sortedNumbers)
+        {
+            Console.WriteLine(num);
+        }
+
+        // Filtering even numbers
+        var evenNumbers = sortedNumbers.Where( num => num%2 == 0);
+        Console.WriteLine("Filtering by even numbers:");
+        foreach(var num in evenNumbers)
+        {
+            Console.WriteLine(num);
+        }
+
+        // Find the sum of even Numbers
+        Console.WriteLine("Finding the sum of even Numbers: ");
+        var sumOfEvenNumbers = evenNumbers.Sum();
+        Console.WriteLine(sumOfEvenNumbers);
+
         
     }
 
