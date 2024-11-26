0433a9e1ade85df5ad8bbed980f1ad40e409f6e2
neojarvis <techadmin@iamneo.ai>
2024-11-26 07:43:21 +0000

    Auto-save-commit-26/11/2024--13:13:21

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,19 +1,42 @@
 ﻿using System;
-using System.Threading.Tasks;
-
-class Program
+namespace MultipleExceptionHandling
 {
-    static async Task Main(string[] args)
+    class Program
     {
-        Console.WriteLine("program started");
-        await SimulateTaskAsync();
-        Console.WriteLine("program ended.");
-    }
+        static void Main(string[] args)
+        {
+            try{
+                Console.WriteLine("Enter the size of the array");
+                int size = int.Parse(Console.ReadLine());
 
-    static async Task SimulateTaskAsync()
-    {
-        Console.WriteLine("Task Started...");
-        await Task.Delay(2000);
-        Console.WriteLine("Task Completed.");
+                int[] numbers = new int[size];
+                Console.WriteLine("Enter the numbers for the array:");
+                for(int i = 0;i <size;i++)
+                {
+                    Console.WriteLine($"Enter the numbers of the array {i}");
+                    numbers[i] = int.Parse(Console.ReadLine());
+                }
+
+                Console.WriteLine("Enter the index of numerator");
+                int numeratorIndex = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the index of demoninator");
+                int denominatorIndex = int.Parse(Console.ReadLine());
+
+                int numerator = numbers[numeratorIndex];
+                int denominator = numbers[denominatorIndex];
+
+                int result = numerator/denominator;
+                Console.WriteLine($"Result: {numerator}/{denominator} = {result}");
+            }
+            catch(IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("Error: Index out of R")
+
+            }
+            finally{
+
+            }
+        }
     }
 }
