179f9f028481e7e700ffef652da99c627636cdad
neojarvis <techadmin@iamneo.ai>
2024-11-26 12:19:52 +0000

    Auto-save-commit-26/11/2024--17:49:50

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,51 +1,14 @@
 ﻿using System;
-namespace MultipleExceptionHandling
+using System.Linq;
+using System.Collections.Generic;
+
+class Program
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            try{
-                Console.WriteLine("Enter the size of the array");
-                int size = int.Parse(Console.ReadLine());
-
-                int[] numbers = new int[size];
-                Console.WriteLine("Enter the numbers for the array:");
-                for(int i = 0;i <size;i++)
-                {
-                    Console.WriteLine($"Enter the numbers of the array {i}");
-                    numbers[i] = int.Parse(Console.ReadLine());
-                }
-
-                Console.WriteLine("Enter the index of numerator");
-                int numeratorIndex = int.Parse(Console.ReadLine());
-
-                Console.WriteLine("Enter the index of demoninator");
-                int denominatorIndex = int.Parse(Console.ReadLine());
-
-                int numerator = numbers[numeratorIndex];
-                int denominator = numbers[denominatorIndex];
-
-                int result = numerator/denominator;
-                Console.WriteLine($"Result: {numerator}/{denominator} = {result}");
-            }
-            catch(IndexOutOfRangeException ex)
-            {
-                Console.WriteLine("Error: Index out of Range! Please enter a valid Index.");
-                Console.WriteLine($"Exception Details : {ex.Message}");
-
-            }
-            catch(DivideByZeroException ex){
-                Console.WriteLine("Error:Cannot divide by zero");
-                Console.WriteLine($"Exception Details : {ex.Message}");
-            }
-            catch(FormatException ex){
-                Console.WriteLine("Error:Invalid input! Please enter only numbers.");
-                Console.WriteLine($"Exception Details : {ex.Message}");
-            }
-            finally{
-                    Console.WriteLine("program execution is completed.");
-            }
-        }
+        List<int> numbers = new List<int>
+        {10,1,7,9,2,5,3,4,8};
+        
     }
+
 }
