5ce4af29eb6c091fd0f1f77adc7cc64a93265df9
neojarvis <techadmin@iamneo.ai>
2024-11-26 03:41:33 +0000

    Auto-save-commit-26/11/2024--09:11:33

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -3,6 +3,18 @@
 class Program{
     public static void Main()
     {
-        Stack<string> browserHistory = new
+        HashSet<string> uniqueitems = new HashSet<string>
+        {
+            "Pen","Notebook","Eraser"
+        };
+        uniqueitems.Add("Pen");
+        uniqueitems.Add("Eraser");
+        uniqueitems.Add("Marker");
+
+        foreach(string item in uniqueitems)
+        {
+            Console.WriteLine(item);
+        }
+        
     }
 }
