681381d6687c1179c385790b2f88710d44c70dc8
neojarvis <techadmin@iamneo.ai>
2024-11-29 03:59:00 +0000

    Auto-save-commit-29/11/2024--09:28:58

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>
+        {10,1,7,9,2,6,5,3,4,8};
+
+        // Sort the numbers in ascending order
+        var sortedNumbers = numbers.OrderBy(num => num);
+        // 
+    }
+}
