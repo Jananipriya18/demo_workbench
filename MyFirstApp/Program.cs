9a7de6a2759be7d7cd0fadc385645a79cbcbd56b
neojarvis <techadmin@iamneo.ai>
2024-12-02 03:00:25 +0000

    Auto-save-commit-02/12/2024--08:30:25

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -6,6 +6,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        HashSet<ISignatureTypeProvider rollnumbers = new HashSet<int>{}
+        HashSet<int> rollnumbers = new HashSet<int>{100,101,102,103};
+        rollnumbers.Add(104);
+        rollnumbers.Add(100);
+
+        Console.WriteLine("Unique elements in hashSet");
+        foreach(int num in rollnumbers)
+        {
+            Console.WriteLine(num);
+        }
+        
     }
 }
