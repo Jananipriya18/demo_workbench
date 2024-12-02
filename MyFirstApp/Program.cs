92acee7af808d7dc41dd15716f76e879cff3bc4b
neojarvis <techadmin@iamneo.ai>
2024-12-02 02:15:25 +0000

    Auto-save-commit-02/12/2024--07:45:25

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,34 +1,13 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
+
 class Program
 {
     static void Main(string[] args)
     {
-        List<int> numbers = new List<int>
-        {10,1,7,9,2,6,5,3,4,8};
+        int[] numbers = {1,2,3,4,5};
+        Console.WriteLine($"Last element: {numbers[4]}");
 
-        // Sort the numbers in ascending order
-        var sortedNumbers = numbers.OrderBy(num => num);
-        // Filtering by Even numbers
-        var evenNumbers = sortedNumbers.Where(num=> num%2 == 0);
-        // Sum of even Numbers
-        var sumOfEvenNumbers = evenNumbers.Sum();
-
-        Console.WriteLine("Sorted even numbers:");
-        foreach(var num in evenNumbers)
-        {
-            Console.WriteLine(num);
-        }
-
-        Console.WriteLine("Ascending order");
-        foreach(var num in sortedNumbers)
-        {
-            Console.WriteLine(num);
-        }
-
-        Console.WriteLine(sumOfEvenNumbers);
-
-
+        numbers[1] = 10;
+        Console.WriteLine($"Modified element:")
     }
 }
