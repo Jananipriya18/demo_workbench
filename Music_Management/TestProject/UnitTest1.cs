a1be783d7bed1b1983e119d38b1bbf98f6f6e81c
neojarvis <techadmin@iamneo.ai>
2024-12-05 16:24:25 +0000

    Auto-save-commit-05/12/2024--21:54:23

--- Music_Management/TestProject/UnitTest1.cs
+++ Music_Management/TestProject/UnitTest1.cs
@@ -1,15 +1,194 @@
-namespace TestProject;
+using NUnit.Framework;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using dotnetapp.Models;
+using System.Reflection;
 
-public class Tests
+namespace dotnetapp.Tests
 {
-    [SetUp]
-    public void Setup()
+    [TestFixture]
+    public class MusicRecordsControllerTests
     {
-    }
+        private const string MusicRecordServiceName = "MusicRecordService";
+        private const string MusicRecordRepositoryName = "MusicRecordRepository";
+        private HttpClient _httpClient;
+        private Assembly _assembly;
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        private MusicRecord _testMusicRecord;
+
+        [SetUp]
+        public async Task Setup()
+        {
+            _assembly = Assembly.GetAssembly(typeof(dotnetapp.Services.IMusicRecordService));
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("http://localhost:8080"); // Base URL of your API
+
+            // Create a new test music record before each test case
+            _testMusicRecord = await CreateTestMusicRecord();
+        }
+
+        private async Task<MusicRecord> CreateTestMusicRecord()
+        {
+            var newMusicRecord = new MusicRecord
+            {
+                Artist = "Test Artist",
+                Album = "Test Album",
+                Genre = "Test Genre",
+                Price = 19.99m,
+                StockQuantity = 10
+            };
+
+            var json = JsonConvert.SerializeObject(newMusicRecord);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("api/MusicRecord", content);
+            response.EnsureSuccessStatusCode();
+
+            return JsonConvert.DeserializeObject<MusicRecord>(await response.Content.ReadAsStringAsync());
+        }
+
+        [Test]
+        public async Task Test_GetAllMusicRecords_ReturnsListOfMusicRecords()
+        {
+            var response = await _httpClient.GetAsync("api/MusicRecord");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var musicRecords = JsonConvert.DeserializeObject<MusicRecord[]>(content);
+
+            Assert.IsNotNull(musicRecords);
+            Assert.IsTrue(musicRecords.Length > 0);
+        }
+
+        [Test]
+        public async Task Test_GetMusicRecordById_ValidId_ReturnsMusicRecord()
+        {
+            var response = await _httpClient.GetAsync($"api/MusicRecord/{_testMusicRecord.MusicRecordId}");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+            var content = await response.Content.ReadAsStringAsync();
+            var musicRecord = JsonConvert.DeserializeObject<MusicRecord>(content);
+
+            Assert.IsNotNull(musicRecord);
+            Assert.AreEqual(_testMusicRecord.MusicRecordId, musicRecord.MusicRecordId);
+        }
+
+        [Test]
+        public async Task Test_GetMusicRecordById_InvalidId_ReturnsNotFound()
+        {
+            var response = await _httpClient.GetAsync($"api/MusicRecord/999");
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [Test]
+        public async Task Test_AddMusicRecord_ReturnsCreatedResponse()
+        {
+            var newMusicRecord = new MusicRecord
+            {
+                Artist = "Test Artist",
+                Album = "Test Album",
+                Genre = "Test Genre",
+                Price = 19.99m,
+                StockQuantity = 10
+            };
+
+            var json = JsonConvert.SerializeObject(newMusicRecord);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("api/MusicRecord", content);
+            response.EnsureSuccessStatusCode();
+
+            var createdMusicRecord = JsonConvert.DeserializeObject<MusicRecord>(await response.Content.ReadAsStringAsync());
+
+            Assert.IsNotNull(createdMusicRecord);
+            Assert.AreEqual(newMusicRecord.Artist, createdMusicRecord.Artist);
+        }
+
+        [Test]
+        public async Task Test_UpdateMusicRecord_ValidId_ReturnsNoContent()
+        {
+            _testMusicRecord.Artist = "Updated Artist";
+
+            var json = JsonConvert.SerializeObject(_testMusicRecord);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PutAsync($"api/MusicRecord/{_testMusicRecord.MusicRecordId}", content);
+
+            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+        }
+
+        [Test]
+        public async Task   Test_UpdateMusicRecord_ValidId_ReturnsNullRecord()
+        {
+            var response = await _httpClient.DeleteAsync($"api/MusicRecord/{_testMusicRecord.MusicRecordId}");
+
+            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+
+            // Set the test music record to null since it has been deleted
+            _testMusicRecord = null;
+        }
+
+
+        [Test]
+        public void Test_MusicRecordService_Exist()
+        {
+            AssertServiceInstanceNotNull(MusicRecordServiceName);
+        }
+
+        [Test]
+        public void Test_MusicRecordRepository_Exist()
+        {
+            AssertRepositoryInstanceNotNull(MusicRecordRepositoryName);
+        }
+
+        private void AssertServiceInstanceNotNull(string serviceName)
+        {
+            Type serviceType = _assembly.GetType($"dotnetapp.Services.{serviceName}");
+
+            if (serviceType == null)
+            {
+                Assert.Fail($"Service {serviceName} does not exist.");
+            }
+
+            // Create a dummy repository instance to pass to the service constructor
+            Type repositoryType = _assembly.GetType($"dotnetapp.Repository.{MusicRecordRepositoryName}");
+            object repositoryInstance = Activator.CreateInstance(repositoryType);
+            object serviceInstance = Activator.CreateInstance(serviceType, repositoryInstance);
+
+            Assert.IsNotNull(serviceInstance);
+        }
+
+        private void AssertRepositoryInstanceNotNull(string repositoryName)
+        {
+            Type repositoryType = _assembly.GetType($"dotnetapp.Repository.{repositoryName}");
+
+            if (repositoryType == null)
+            {
+                Assert.Fail($"Repository {repositoryName} does not exist.");
+            }
+
+            object repositoryInstance = Activator.CreateInstance(repositoryType);
+            Assert.IsNotNull(repositoryInstance);
+        }
+
+        [TearDown]
+        public async Task Cleanup()
+        {
+            if (_testMusicRecord != null)
+            {
+                var response = await _httpClient.DeleteAsync($"api/MusicRecord/{_testMusicRecord.MusicRecordId}");
+                if (response.StatusCode != HttpStatusCode.NotFound)
+                {
+                    response.EnsureSuccessStatusCode();
+                }
+            }
+            _httpClient.Dispose();
+        }
     }
 }
